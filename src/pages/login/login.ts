87113ce145afef5671271fd6ed5c73e6c58ea132
Luis Alberto Cruz <luicruz.amaya@gmail.com>
2017-06-02 17:09:30 -0500

    Refactor login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,84 +1,24 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController, LoadingController, Loading, IonicPage } from 'ionic-angular';
-import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-
-import { TabsPage } from "../tabs/tabs";
-
-
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
 
-  user:string;
-  password:string;
-  phUser:string = "Usuario";
-
-  loading: Loading;
-  registerCredentials = { email: '', password: '' };
-
-  constructor(private nav: NavController, private auth: AuthServiceProvider, private alertCtrl: AlertController, private loadingCtrl: LoadingController) { }
-
-    public createAccount() {
-    this.nav.push('RegisterPage');
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  public login() {
-    this.showLoading()
-    this.auth.login(this.registerCredentials).subscribe(allowed => {
-      if (allowed) {
-        this.nav.setRoot('HomePage');
-      } else {
-        this.showError("Access Denied");
-      }
-    },
-      error => {
-        this.showError(error);
-      });
-  }
-
-  showLoading() {
-    this.loading = this.loadingCtrl.create({
-      content: 'Please wait...',
-      dismissOnPageChange: true
-    });
-    this.loading.present();
-  }
-
-  showError(text) {
-    this.loading.dismiss();
-
-    let alert = this.alertCtrl.create({
-      title: 'Fail',
-      subTitle: text,
-      buttons: ['OK']
-    });
-    alert.present(prompt);
-  }
-
-  login_(user:string, password:string){
-    let isValidEmail = this.ValidateEmail(user);
-    if (isValidEmail){
-      if(user=="demo@getin.mx" && password=="admin01"){
-        this.nav.push( TabsPage );
-      }
-    }else{
-      this.user="";
-      this.phUser = "Ususario incorrecto, intente de nuevo";
-    }
-  }
-
-  private ValidateEmail( mail:string ) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
-      return (true);
-    }
-    return (false)
-  }
-
-  forgotPassword(){
-    console.log("Se olvido el password");
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
   }
 
 }
