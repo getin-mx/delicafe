e1229158b0793fcaf42a6f4c3f5ddf387d572dcf
Luis Alberto Cruz <luicruz.amaya@gmail.com>
2017-06-05 18:23:48 -0500

    modify image resources

--- src/pages/social/social.ts
+++ src/pages/social/social.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SocialPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-social',
@@ -14,11 +8,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SocialPage {
 
+  socialImages:any = [
+    {
+      imageUri: "assets/img/redes/IMG_3892.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_3922.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_3934.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_3973.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4004.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4021.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4023.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4030.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4060.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4063.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4069.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4135.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4236.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4534.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4645_2.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4705.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4754.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4808.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4869.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4908.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_4920.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_5059.JPG"
+    },{
+      imageUri: "assets/img/redes/IMG_5091.JPG"
+    }
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SocialPage');
-  }
+
 
 }
